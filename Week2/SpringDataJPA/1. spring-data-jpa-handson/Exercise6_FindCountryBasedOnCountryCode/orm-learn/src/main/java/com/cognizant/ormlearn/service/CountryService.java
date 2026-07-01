bf0c766c940cd387abb965e331ec3d155db3b5a5
Nitin Kumar <nk9665985@gmail.com>
2026-07-01 10:09:24 +0530

    Completed Spring Data JPA Exercise 6 - Find Country by Code

---
+++ Week2/SpringDataJPA/1. spring-data-jpa-handson/Exercise6_FindCountryBasedOnCountryCode/orm-learn/src/main/java/com/cognizant/ormlearn/service/CountryService.java
@@ -0,0 +1,39 @@
+package com.cognizant.ormlearn.service;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.cognizant.ormlearn.model.Country;
+import com.cognizant.ormlearn.repository.CountryRepository;
+import com.cognizant.ormlearn.service.exception.CountryNotFoundException;
+
+import jakarta.transaction.Transactional;
+
+@Service
+public class CountryService {
+
+    @Autowired
+    private CountryRepository countryRepository;
+
+    @Transactional
+    public List<Country> getAllCountries() {
+        return countryRepository.findAll();
+    }
+
+    @Transactional
+    public Country findCountryByCode(String countryCode)
+            throws CountryNotFoundException {
+
+        Optional<Country> result = countryRepository.findById(countryCode);
+
+        if (!result.isPresent()) {
+            throw new CountryNotFoundException("Country not found");
+        }
+
+        return result.get();
+    }
+
+}
