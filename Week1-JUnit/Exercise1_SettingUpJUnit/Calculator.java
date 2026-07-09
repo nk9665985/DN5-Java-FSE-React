3499ea24343e18d543e3320d2a166f4b67c7adac
Nitin Kumar <nk9665985@gmail.com>
2026-07-09 15:16:23 +0530

    Reorganize Week 1 modules into a single Week1 folder

--- Week1-JUnit/Exercise1_SettingUpJUnit/Calculator.java
+++
@@ -1,8 +0,0 @@
-package junitdemo;
-
-public class Calculator {
-
-    public int add(int a, int b) {
-        return a + b;
-    }
-}
--- Week1-Mockito/Exercise1_Mocking_And_Stubbing/ExternalApi.java
+++
@@ -1,5 +0,0 @@
-package junitdemo;
-
-public interface ExternalApi {
-    String getData();
-}
